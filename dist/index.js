1b3938617e715a6dddf39faf494f390b60e43348
Abdul El-Saied <abdul.elsaied@berkeley.edu>
2024-02-07 21:25:40 -0800

    add route folder and set up basic routes for first two games

--- dist/index.js
+++ dist/index.js
@@ -4,14 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const flag_memory_1 = __importDefault(require("./routes/flag-memory"));
+const more_or_less_1 = __importDefault(require("./routes/more-or-less"));
 const port = 8000;
 const app = (0, express_1.default)();
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     res.send("HELLO FROM EXPRESS + TS!!!");
 });
-app.get("/hi", (req, res) => {
-    res.send("BBYYE");
-});
+app.use("/flag-memory", flag_memory_1.default);
+app.use("/more-or-less", more_or_less_1.default);
 app.listen(port, () => {
     console.log("listening on port 8000");
 });
--- index.ts
+++ index.ts
@@ -1,15 +1,20 @@
 import express from "express";
+import flagMemoryRouter from "./routes/flag-memory";
+import moreOrLessRouter from "./routes/more-or-less";
+
 const port = 8000;
 
 const app = express();
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.send("HELLO FROM EXPRESS + TS!!!");
 })
 
-app.get("/hi", (req, res) => {
-    res.send("BBYYE");
-})
+app.use("/flag-memory", flagMemoryRouter);
+app.use("/more-or-less", moreOrLessRouter);
+
 
 app.listen(port, () => {
     console.log("listening on port 8000");
