f72ede3219705a2a9f0bd58b1c9c458eec9cf7d3
Abdul El-Saied <abdul.elsaied@berkeley.edu>
2024-02-19 21:03:13 -0800

    add vite and react

---
+++ dist/backend/app.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const index_1 = __importDefault(require("./routes/index"));
+const flagMemory_1 = __importDefault(require("./routes/flagMemory"));
+const moreOrLess_1 = __importDefault(require("./routes/moreOrLess"));
+const port = 8000;
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use("/", index_1.default);
+app.use("/flag-memory", flagMemory_1.default);
+app.use("/more-or-less", moreOrLess_1.default);
+app.set("view engine", "ejs");
+app.listen(port, () => {
+    console.log("listening on port 8000");
+});
+exports.default = app;
