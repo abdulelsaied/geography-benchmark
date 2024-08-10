15561174c57b2b9843ac3caf3554c57112861985
Abdul El-Saied <abdul.elsaied@berkeley.edu>
2024-08-10 10:25:55 -0700

    update header component

--- dist/frontend/src/components/MenuButton.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const MenuButtons = ({ text, buttonFunction }) => {
-    return ((0, jsx_runtime_1.jsx)("button", { className: "bg-transparent hover:bg-blue-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded", onClick: () => buttonFunction(), children: text }));
-};
-exports.default = MenuButtons;
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,37 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import NavButton from './NavButton';
-import { IoMdHelp, IoIosStats, IoIosInformationCircle } from "react-icons/io";
-
+import { IoMdHelp, IoIosStats, IoIosInformationCircle } from 'react-icons/io';
 
 const Header: React.FC = () => {
-    return (
-        <>
-            <div className="flex flex-row text-white pr-4 items-end mb-2">
-                <div className="pl-4 pr-4 border-r-2 border-white">
-                    <h1 className="text-4xl font-bold">GE🌎GRAPHY</h1>
-                    <h1 className="text-4xl font-bold">BENCHMARK</h1>
-                </div>
-                <div className="ml-4">
-                    <h4 className="text-xl">test your geo skills</h4>
-                    <h4 className="text-xl">inspired by HumanBenchmark</h4>
-                </div>
-            </div>
-            <div className = "flex flex-row ">
-                <NavButton 
-                    text = "help"
-                    icon = {<IoMdHelp/>}
-                />
-                <NavButton 
-                    text = "stats"
-                    icon = {<IoIosStats/>}
-                />
-                <NavButton 
-                    text = "about"
-                    icon = {<IoIosInformationCircle/>}
-                />
-            </div>
-        </>
-    );
-}
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div 
+        className="flex flex-row text-white pr-4 items-end mb-2"
+      >
+        <div 
+            className="pl-4 pr-4 border-r-2 border-white cursor-pointer"
+            onClick={() => navigate('/')} // Navigate to home page on click
+        >
+          <h1 className="text-4xl font-bold">GE🌎GRAPHY</h1>
+          <h1 className="text-4xl font-bold">BENCHMARK</h1>
+        </div>
+        <div className="ml-4">
+          <h4 className="text-xl">test your geo skills</h4>
+          <h4 className="text-xl">inspired by HumanBenchmark</h4>
+        </div>
+      </div>
+      <div className="flex flex-row">
+        <NavButton 
+          text="help"
+          icon={<IoMdHelp />}
+        />
+        <NavButton 
+          text="stats"
+          icon={<IoIosStats />}
+        />
+        <NavButton 
+          text="about"
+          icon={<IoIosInformationCircle />}
+        />
+      </div>
+    </>
+  );
+};
 
 export default Header;
