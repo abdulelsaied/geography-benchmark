3f9419c380936fe46ad9af1ad26506ed9728b59c
Abdul El-Saied <abdul.elsaied@berkeley.edu>
2025-01-08 13:09:15 -0800

    update connection string

--- backend/database.ts
+++ backend/database.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URI
-});
+    connectionString: "postgres://default:iUHxRvbW73KV@ep-fancy-surf-a64wcnei-pooler.us-west-2.aws.neon.tech:5432/verceldb?sslmode=require"
+})
 
 export default pool;
