52631d3d720e084a29e9c1ab6db535125d60fcb4
Abdul El-Saied <abdul.elsaied@berkeley.edu>
2025-01-08 13:18:27 -0800

    add vercel env vars

--- backend/database.ts
+++ backend/database.ts
@@ -1,9 +1,7 @@
 import { Pool } from "pg"
-import dotenv from "dotenv";
-dotenv.config();
 
 const pool = new Pool({
-    connectionString: "postgres://default:iUHxRvbW73KV@ep-fancy-surf-a64wcnei-pooler.us-west-2.aws.neon.tech:5432/verceldb?sslmode=require"
+    connectionString: process.env.DATABASE_CONNECTION_STRING
 })
 
 export default pool;
